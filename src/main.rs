3cf141b87a63073432d1cf5515cd04ce99674a86
justinbather <justinbather@hotmail.com>
2023-10-17 18:30:22 -0400

    clean up

--- src/main.rs
+++ src/main.rs
@@ -17,62 +17,6 @@ fn main(){
 
     let config = config::Config::new(&args).unwrap();
 
-
-    // let mut focus_duration = String::new(); 
-
-    // let mut num_intervals = String::new();
-    // let mut break_duration = String::new();
-
-    // println!("How long do you want your focus sessions?");
-    
-    // io::stdin()
-    //     .read_line(&mut focus_duration)
-    //     .expect("Invalid input");
-
-    // let focus_duration: u64 = match focus_duration.trim().parse() {
-    //     Ok(time) => time,
-    //     Err(_) => {
-    //         println!("Not an integer");
-    //         process::exit(1)
-    //         },
-    // };
-
-    // println!("How long should your breaks be?");
-
-    // io::stdin()
-    // .read_line(&mut break_duration)
-    // .expect("Invalid input for break duration");
-
-    // let break_duration: u64 = match break_duration.trim().parse() {
-    //     Ok(time) => time,
-    //     Err(_) => {
-    //         println!("Not an integer for break duration");
-    //         process::exit(1)
-    //     }
-    // };
-
-    // println!("How many intervals would you like? Min 1");
-
-    // io::stdin()
-    // .read_line(&mut num_intervals)
-    // .expect("Invalid entry for intervals");
-
-    // let num_intervals: u64 = match num_intervals.trim().parse() {
-    //     Ok(num) => num,
-    //     Err(_) => {
-    //         println!("Not a valid interval");
-    //         process::exit(1)
-    //     },
-    // };
-
-
-    
-
-    // println!("{:#?}", config);
-    
-
-
-
     let current = time::Instant::now();
 
     let duration: u64 = config.focus_duration / 1000;
