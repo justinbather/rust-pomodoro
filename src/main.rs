8870ff545fa5ffefa03293068548edaa19b5af79
justinbather <justinbather@hotmail.com>
2023-10-17 11:48:38 -0400

    feat: adds notifications, needs work

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@
 */
 
 use std::io;
-use std::env;
-use std::time;
 use std::process;
+use std::time;
 
-fn main() {
+use notify_rust::Notification;
+use notify_rust::NotificationHandle;
+
+fn main(){
     
     let mut timer = String::new(); 
 
@@ -24,7 +26,7 @@ fn main() {
         Ok(time) => time,
         Err(_) => {
             println!("Not an integer");
-            return;
+            process::exit(1)
             },
     };
 
@@ -32,29 +34,66 @@ fn main() {
 
     let current = time::Instant::now();
 
-    let mut duration: u64 = &timer / 1000;
+    let duration: u64 = &timer / 1000;
 
 
 
     // Prints correctly
     //
     println!("{:?}", current);
+    let notification_result = send_notification();
+    match notification_result {
+        Ok(_) => println!("Notification success: {:?}", notification_result),
+        Err(err) => println!("Eror sending notification: {}", err)
+    }
 
     loop {
-        if (current.elapsed().as_secs() >= duration) {
+        if current.elapsed().as_secs() >= duration {
             break;
         }
     }
     
     println!("Timer finished after {:?}", current.elapsed().as_secs());
+    
 
+    // let bundle = get_bundle_identifier_or_default("");
+    // println!("{:?}", &bundle);
+    // match set_application(&bundle) {
+    //     Ok(_) => {
+    //         println!("Application set success:");
 
+    //         let notification = send_notification(
+    //             "NOW",
+    //             Some("Subtitle"),
+    //             "Without subtitle",
+    //             Some(Notification::new().sound("Blow"))
+    //         );
 
+    //         match notification {
+    //             Ok(_) => {
+    //                 println!("Notification sent successfully")
+    //             },
+    //             Err(e) => println!("Error sending notification: {}", e)
+    //         };
 
-
-
-
-
-
-
+    //     },
+    //     Err(e) => println!("Error setting application: {}", e)
+    // }
+    
 }
+
+fn send_notification() -> Result<(), Box<dyn std::error::Error>> {
+    let notification = Notification::new()
+    .summary("Safari Crashed")
+    .subtitle("subtitle")
+    
+    .body("Just kidding, this is just the notify_rust example.")
+    .show()?;
+
+    println!("{:?}", notification);
+
+    
+    Ok(())
+}
+
+
